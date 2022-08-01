7fcda7916a15504aa25606e98cbcfcce356b13af
Kooreis <kooreis.official@gmail.com>
2022-07-31 23:00:00 -0500

    This update calculates the BMI using the formula weight divided by height squared and prints the result.

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -1,3 +1,2 @@
-def calculate_bmi():
-    height = float(input("Enter your height in meters: "))
-    weight = float(input("Enter your weight in kg: "))
+bmi = weight / (height ** 2)
+    print("Your BMI is: {0} and you are: ".format(bmi), end='')
